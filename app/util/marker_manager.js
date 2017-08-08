c34d77cfbf90dc8e3774e9ad53291f24aebb809a
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-08-08 10:04:05 -0700

    complete marker manager for google map

---
+++ app/util/marker_manager.js
@@ -0,0 +1,87 @@
+export default class MarkerManager {
+  constructor(map, infowindow, handleClick) {
+    this.map = map;
+    this.markers = {};
+    this.removeMarker = this.removeMarker.bind(this);
+    this.infowindow = infowindow;
+    this.handleClick = handleClick;
+  }
+
+  updateMarkers(restaurants) {
+    const restaurantsObj = {};
+
+    restaurants.forEach((restaurant) => {
+      restaurantsObj[restaurant.id] = restaurant;
+    });
+
+    restaurants
+      .filter(restaurant => !this.markers[restaurant.id])
+      .forEach((newRestaurant) => {
+      this.createMarkerFromRoom(newRestaurant, this.infowindow, this.handleClick);
+    });
+    Object.keys(this.markers)
+      .filter(restaurantId => !restaurantsObj[restaurantId])
+      .forEach((restaurantId) => this.removeMarker(this.markers[restaurantId]));
+  }
+
+  removeMarker(marker) {
+    this.markers[marker.restaurantId].setMap(null);
+    delete this.markers[marker.restaurantId];
+  }
+
+  iconFromDisplayPos(restaurant) {
+    //get three colors and base icon off of that
+    const icon = "";
+    if (restaurant.displayPosition === 1) {
+      icon = "htttp://1";
+    } else if (restaurant.displayPosition === 2) {
+      icon = "htttp://2";
+    } else {
+      icon = "htttp://3";
+    }
+    return icon;
+  }
+
+  createMarkerFromRoom(restaurant, infowindow, handleClick) {
+    const pos = new google.maps.LatLng(restaurant.lat, restaurant.lng);
+    const icon = ;
+    const marker = new google.maps.Marker({
+      position: pos,
+      map: this.map,
+      icon: this.iconFromDisplayPos(restaurant),
+      animation: google.maps.Animation.DROP,
+      restaurantId: restaurant.id
+    });
+    this.markers[marker.restaurantId] = marker;
+
+    marker.addListener('click', function () {
+      handleClick(restaurant.id);
+    });
+
+    infowindow.addListener('click', function () {
+      handleClick(restaurant.id);
+    });
+
+    //change this to be restaurant specific
+    // var contentString = '<div id="content">'+
+    //   '<div id="siteNotice">'+
+    //   '</div>'+
+    //   `<img src=${room.pic_url} height="100px" width="150px"></img>` +
+    //   `<h4 id="firstHeading" class="firstHeading">$${room.price} ${room.title}</h4>`+
+    //   `<h5>${room.room_type} · ${room.beds} beds</h5>`+
+    //   '<div id="bodyContent">'+
+    //   '</div>'+
+    //   '</div>';
+
+    marker.addListener('mouseover', function () {
+      infowindow.close();
+      infowindow.setContent(contentString);
+      infowindow.open(marker.map, marker);
+    });
+
+    marker.addListener('mouseout', function () {
+      infowindow.close();
+    });
+  }
+
+}
