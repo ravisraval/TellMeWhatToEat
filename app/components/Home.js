a39cc050ac81c64675417583c53d4de070d040b3
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-08-07 19:19:19 -0700

    render searchbox on landing page

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,50 +3,13 @@ import { connect } from 'react-redux';
 import Messages from './Messages';
 // import Map from './Landing_page/Map';
 // import AutoSearch from './Landing_page/auto_complete_container';
-import ComboMapSearch from './Landing_page/combo_map_search';
-import FindRestaurants from './find_restaurants';
+import ComboMapSearch from './Landing_page/combo_map_search.js';
+// import FindRestaurants from './find_restaurants';
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="container-fluid">
-        <Messages messages={this.props.messages}/>
-        <div className="row">
-          <div className="col-sm-4">
-            <div className="panel">
-              <div className="panel-body">
-                <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-                  mollis euismod. Donec sed odio dui.</p>
-                <a href="#" role="button" className="btn btn-default">View details</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="panel">
-              <div className="panel-body">
-                <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-                  mollis euismod. Donec sed odio dui.</p>
-                <a href="#" role="button" className="btn btn-default">View details</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="panel">
-              <div className="panel-body">
-                <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-                  mollis euismod. Donec sed odio dui.</p>
-                <a href="#" role="button" className="btn btn-default">View details</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <FindRestaurants/>
+      <div className="landing-page-container">
         <ComboMapSearch/>
       </div>
     );
@@ -60,3 +23,41 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(Home);
+
+
+//
+// <div className="row">
+//   <div className="col-sm-4">
+//     <div className="panel">
+//       <div className="panel-body">
+//         <h3>Heading</h3>
+//         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+//           mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+//           mollis euismod. Donec sed odio dui.</p>
+//         <a href="#" role="button" className="btn btn-default">View details</a>
+//       </div>
+//     </div>
+//   </div>
+//   <div className="col-sm-4">
+//     <div className="panel">
+//       <div className="panel-body">
+//         <h3>Heading</h3>
+//         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+//           mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+//           mollis euismod. Donec sed odio dui.</p>
+//         <a href="#" role="button" className="btn btn-default">View details</a>
+//       </div>
+//     </div>
+//   </div>
+//   <div className="col-sm-4">
+//     <div className="panel">
+//       <div className="panel-body">
+//         <h3>Heading</h3>
+//         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+//           mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+//           mollis euismod. Donec sed odio dui.</p>
+//         <a href="#" role="button" className="btn btn-default">View details</a>
+//       </div>
+//     </div>
+//   </div>
+// </div>
--- app/components/Landing_page/combo_map_search.js
+++ app/components/Landing_page/combo_map_search.js
@@ -4,64 +4,63 @@ import ReactDOM from 'react-dom';
 class ComboMapSearch extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      lng: null,
+      lat: null
+    };
   }
 
+  componentDidUpdate() {
+    let self = this;
+    const geocoder = new google.maps.Geocoder;
+
+    geocoder.geocode({'location': this.state}, function(results, status) {
+       if (status === 'OK') {
+         if (results[1]) {
+           self.searchLocation.value = results[1].formatted_address;
+         } else {
+           window.alert('No results found');
+         }
+       } else {
+         window.alert('Geocoder failed due to: ' + status);
+       }
+     });
+  }
+
+  getLocation() {
+    let self = this;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      self.setState({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude});
+      }, (error) => alert(error.message) );
+  }
+
+
   componentDidMount() {
-    let searchLocation = document.getElementById('searchLocation');
+    let searchLocation = this.searchLocation;
     let options = {
       types: ['address']
     };
+
     let autoComplete = new google.maps.places.Autocomplete(searchLocation, options);
-
-     const map = this.refs.map;
-     const mapOptions = {
-       center: {lat: 37.773972,
-       lng: -122.431297},
-       zoom: 12
-     };
-     this.map = new google.maps.Map(map, mapOptions);
-
-     this.listeners(autoComplete, map);
+    this.searchLocation = document.getElementById('searchLocation');
+    this.getLocation();
+    this.listeners(autoComplete);
   }
-  //
-  // getCurrentLocation() {
-  //   navigator.geolocation.getCurrentPosition((position)=>{
-  //     return {lat: position.coords.latitude, lng: position.coords.longitude};
-  //   });
-  // }
 
-  listeners(autoComplete, map){
+  listeners(autoComplete){
     let self = this;
 
     autoComplete.addListener('place_changed', function() {
-
       let place = autoComplete.getPlace();
       const long = place.geometry.viewport["b"]["b"];
       const latt = place.geometry.viewport["f"]["b"];
       var pos = {lat: latt, lng: long};
-      console.log(long, latt);
       const mapOptions = {
-        center: pos, // this is area 51
+        center: pos, // this SF
         zoom: 14
       };
-      //  autoComplete.bindTo('bounds', self.map);
-      //
-      const mapRerender = new google.maps.Map(map, mapOptions);
-      var marker = new google.maps.Marker({
-        position: pos,
-        map: mapRerender,
-      });
-      self.map.setCenter(pos, 14);
-      // self.props.updateGeoLocation({
-      // lat: place.geometry.viewport["f"]["b"],
-      // lng: place.geometry.viewport["b"]["b"],
-      // address: place.formatted_address});
-      //
-      //
-      //
-      //
-
-
     });
   }
 
@@ -69,9 +68,7 @@ class ComboMapSearch extends React.Component {
     return (
       <div>
         <input id='searchLocation' placeholder='Enter your address'></input>
-        <div id="map" className="map" ref="map"> Map </div>
       </div>
-
     );
   }
 
