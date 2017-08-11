4c6a7401debb041a30c9b129a33bb8278414559c
Syrie Bianco <syriebianco@gmail.com>
2017-08-11 05:59:26 -0700

    Reorganize data file structure

--- docs/data/xlsx_to_json.js
+++
@@ -1,8 +0,0 @@
-const convertExcel = require('excel-as-json').processFile;
-
-
-convertExcel('./data_breakdown.xlsx', './data.json');
-
-convertExcel('./categories/categories.xlsx', './categories/categories.json');
-convertExcel('./answers/answers.xlsx', './answers/answers.json');
-convertExcel('./questions/questions.xlsx', './questions/questions.json');
