164db03bafea5299b08dfdfee1f9bf71a7f79f9b
Syrie Bianco <syriebianco@gmail.com>
2017-08-11 05:29:45 -0700

    Configure xlsx_to_json and return

---
+++ docs/data/xlsx_to_json.js
@@ -0,0 +1,8 @@
+const convertExcel = require('excel-as-json').processFile;
+
+
+convertExcel('./data_breakdown.xlsx', './data.json');
+
+convertExcel('./categories/categories.xlsx', './categories/categories.json');
+convertExcel('./answers/answers.xlsx', './answers/answers.json');
+convertExcel('./questions/questions.xlsx', './questions/questions.json');
