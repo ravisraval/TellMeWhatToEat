10c14d691991fb3fe37e3a16c079c62c421ff093
Syrie Bianco <syriebianco@gmail.com>
2017-08-11 20:45:29 -0700

    try to seed files

--- seeds/questions.js
+++ seeds/questions.js
@@ -1,16 +1,16 @@
-const questions = require('../docs/data/questions/questions.json');
-
+// const questions = require('../docs/data/questions/questions.json');
+//
 exports.seed = function(knex, Promise) {
-
-  // Begin promises by deleting ALL existing entries
-  const promises = [knex('questions').del()];
-
-  questions.forEach(el => {
-    promises.push(knex('questions').insert(el));
-  });
-
-  return Promise.all(promises)
-    .then((success) => console.log("success", success))
-    .catch( (err) => console.log("error", err));
-
+//
+//   // Begin promises by deleting ALL existing entries
+//   const promises = [knex('questions').del()];
+//
+//   questions.forEach(el => {
+//     promises.push(knex('questions').insert(el));
+//   });
+//
+//   return Promise.all(promises)
+//     .then((success) => console.log("success", success))
+//     .catch( (err) => console.log("error", err));
+//
 };
--- seeds/test.js
+++ seeds/test.js
@@ -1,3 +1,21 @@
 const questions = require('../docs/data/questions/questions.json');
 
-questions.forEach(el => console.log(el));
+exports.seed = function(knex, Promise) {
+
+  // Begin promises by deleting ALL existing entries
+  const promises = [knex('questions').del()];
+
+
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('questions').del(),
+
+    // Insert new entries
+    knex('questions').insert({
+        "id": 1,
+        "body": "Hot or Cold",
+        "type": "options",
+        "tier": 1,
+      })
+  );
+};
