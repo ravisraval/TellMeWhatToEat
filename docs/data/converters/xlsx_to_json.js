6eb1ca614114406cc4981f6a2eee6bc6a986b393
Syrie Bianco <syriebianco@gmail.com>
2017-08-11 21:17:19 -0700

    Rework excel-to-json file

--- docs/data/converters/xlsx_to_json.js
+++ docs/data/converters/xlsx_to_json.js
@@ -1,8 +1,9 @@
 const convertExcel = require('excel-as-json').processFile;
 
 
-convertExcel('../data_breakdown.xlsx', './data.json');
+convertExcel('./data_breakdown.xlsx', './data.json');
 
 convertExcel('../categories/categories.xlsx', './categories/categories.json');
 convertExcel('../answers/answers.xlsx', './answers/answers.json');
 convertExcel('../questions/questions.xlsx', './questions/questions.json');
+console.log("hi");
